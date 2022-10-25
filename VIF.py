3c779824384e0062ad775857e64111ffa71f560b
Stephen <tama82045@gmail.com>
2022-10-25 20:20:06 +0800

    v1.0.1

--- VIF.py
+++ VIF.py
@@ -5,13 +5,8 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
-pd.set_option('display.max_rows',None)
-#pd.set_option('display.max_columns',None)
 
 
-
-factor1=pd.read_csv("C:/Users/Owner/factor_strategy/src/factor_price_data/after_pretreatment/fx_daily_active_addresses/ftx_daily_active_addresses_pre_week.txt", sep = ",", encoding = "utf-8", engine = "c")
-factor2=pd.read_csv("C:/Users/Owner/factor_strategy/src/factor_price_data/after_pretreatment/fx_ex_flow/ftx_exchange_balance_pre_week.txt", sep = ",", encoding = "utf-8", engine = "c")
 
 factor1['Datetime']=pd.to_datetime(factor1['Datetime'])
 factor1=factor1.set_index(factor1.Datetime).drop(columns={'Datetime'})
@@ -37,13 +32,13 @@ for i in range(len(idx)):
     vif.append([variance_inflation_factor(ck, j) for j in range(ck.shape[1])])
 VIF=pd.DataFrame(data=vif,columns=['cons','factor1','factor2'],index=idx)
 
-'''
+
 plt.plot(VIF.index,VIF['factor1'].values,color = 'lightblue')
 plt.show()
 plt.plot(VIF.index,VIF['factor2'].values,color = 'red')
 plt.xticks(rotation='vertical')
 plt.show()
-'''
+
 
 
 
